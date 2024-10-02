34a032b40e188bf6a26337a74c9f7aa491def974
FrancescoRuscelli <francescoruscelli24@gmail.com>
2024-10-02 18:13:11 +0200

    added possibility to dynamically change obstacles parameters from ROS topic

--- obstacles.py
+++ obstacles.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
+from heapq import merge
 
 from visualization_msgs.msg import Marker, MarkerArray
 import rospy
@@ -9,6 +10,9 @@ from horizon_navigation.pyObstacle import CasadiObstacle, SphereObstacle
 import time
 from dataclasses import dataclass, field
 import random
+from std_msgs.msg import Float64, Int64, Float64MultiArray
+from obstacles_ros_server import ObstaclesROSServer
+import multiprocessing
 
 
 @dataclass
@@ -32,6 +36,25 @@ class ObstacleMapParameters:
         if self.robot_sphere_publisher_name is None:
             self.robot_sphere_publisher_name = self.input_topic_name
 
+    def getParam(self, param_name):
+        try:
+            # Dynamically access the attribute if it exists
+            return getattr(self, param_name)
+        except AttributeError:
+            # Raise an error if the attribute doesn't exist
+            raise AttributeError(f"Parameter '{param_name}' does not exist in ObstacleMapParameters.")
+
+    def setParam(self, param_name, value):
+        try:
+            # Check if the attribute exists
+            if hasattr(self, param_name):
+                # Dynamically set the attribute value
+                setattr(self, param_name, value)
+            else:
+                raise AttributeError(f"Parameter '{param_name}' does not exist in ObstacleMapParameters.")
+        except AttributeError as e:
+            raise e
+
 
 class ObstacleGeneratorWrapper:
 
@@ -45,6 +68,9 @@ class ObstacleGeneratorWrapper:
         self.map_parameters = dict()
 
         self.f_obs_grid = 0  # function of the inputs
+
+
+
 
         self.map_parameters["velodyne_map"] = ObstacleMapParameters(input_topic_name="/costmap_node/costmap/costmap",
                                                                     robot_sphere_radius=np.array([0.75, 0.75]),
@@ -85,14 +111,77 @@ class ObstacleGeneratorWrapper:
 
         self.obs_origin_par_dict = dict()  # dict of origin parameters for each obstacle
         self.obs_weight_par_dict = dict()  # dict of weight parameters for each obstacle
+        self.obs_radius_par_dict = dict()  # dict of radius parameters for each obstacle
+        self.robot_sphere_par_dict = dict() # dict of radius parameters for each sphere approximation of robot
 
-        self.obstacle_distances = dict()
+        # self.obstacle_distances = dict()
 
         self.__init_obstacle_generators()
         self.__init_markers()
         self.__init_ros_publisher()
 
         self.base_fk = kin_dyn.fk('base_link')
+
+        self.variable_sub_name = []
+
+        self.reconfigurable_topics_name = [("max_obs_num", Int64), ("obstacle_radius", Float64), ("weight_cost_obs", Float64), ("robot_sphere_radius", Float64MultiArray)]
+
+        self.__init_subscribers(self.map_parameters.keys(), self.reconfigurable_topics_name)
+
+        self.variable_change_flag = dict()
+        for suffix in self.map_parameters.keys():
+            self.variable_change_flag[suffix] = dict()
+            for param_name, param_type in self.reconfigurable_topics_name:
+                self.variable_change_flag[suffix][param_name] = False
+
+
+
+    def __init_subscribers(self, suffix_names, params):
+
+        self.subscribers = dict()
+        for suffix in suffix_names:
+            self.subscribers[suffix] = list()
+
+            for param_name, param_type in params:
+
+                topic_name = f'/{suffix}/{param_name}'
+                sub = rospy.Subscriber(topic_name, param_type, self.__create_callback(suffix, param_name))
+                self.subscribers[suffix].append(sub)
+                rospy.loginfo(f"Subscriber for {topic_name} ({param_type}) is ready.")
+
+        
+    def __create_callback(self, suffix_name, topic_name):
+
+        def update_variables_callback(msg):
+            """
+            Callback function to update the internal variables based on the received message.
+            """
+
+
+            current_value = msg.data
+            last_value = self.map_parameters[suffix_name].getParam(topic_name)
+
+            if isinstance(msg, Float64MultiArray):
+                # Update only if the new message is different from the last received one
+                if len(current_value) != len(last_value):
+                    raise Exception(f'wrong dimension of parameter inserted {len(current_value)} != {len(last_value)}')
+
+                if len(current_value) >= 1:
+                    for a, b in zip(current_value, last_value):
+                        if a != b:
+                            rospy.loginfo(f"Received update of {suffix_name}/{topic_name}: {msg.data}")
+                            self.map_parameters[suffix_name].setParam(topic_name, np.array(msg.data))
+
+            else:
+
+                if current_value != last_value:
+                    rospy.loginfo(f"Received update of {suffix_name}/{topic_name}: {msg.data}")
+                    self.variable_change_flag[suffix_name][topic_name] = True
+
+                    # Update the internal variables
+                    self.map_parameters[suffix_name].setParam(topic_name, msg.data)
+
+        return update_variables_callback
 
     def __init_obstacle_generators(self):
 
@@ -118,9 +207,14 @@ class ObstacleGeneratorWrapper:
             robot_sphere_origin_absolute = robot_origin + map_param.robot_sphere_origin
 
             # distances from obstacles in each layer are computed from one or more sphere (approximation of the robot)
-            self.obstacle_distances[layer_name] = {f"sphere_{sphere_i}": [] for sphere_i in range(len(map_param.robot_sphere_radius))}
+            # self.obstacle_distances[layer_name] = {f"sphere_{sphere_i}": [] for sphere_i in range(len(map_param.robot_sphere_radius))}
+
+            robot_sphere_radius_par = self.prb.createParameter(f'robot_sphere_radius_map_{i_map}', len(map_param.robot_sphere_radius))
+            self.robot_sphere_par_dict[layer_name] = robot_sphere_radius_par
+
             self.obs_origin_par_dict[layer_name] = list()
             self.obs_weight_par_dict[layer_name] = list()
+            self.obs_radius_par_dict[layer_name] = list()
 
             for obs_num in range(map_param.max_obs_num):
                 # add to cost function all the casadi obstacles, parametrized with ORIGIN and WEIGHT
@@ -130,16 +224,21 @@ class ObstacleGeneratorWrapper:
                 obs_weight_par = self.prb.createParameter(f'obs_weight_map_{i_map}_{obs_num}', 1)
                 self.obs_weight_par_dict[layer_name].append(obs_weight_par)
 
+                obs_radius_par = self.prb.createParameter(f'obs_radius_map_{i_map}_{obs_num}', 1)
+                self.obs_radius_par_dict[layer_name].append(obs_radius_par)
+
+
                 # dictionary of "map layers" with obstacles
                 # for each obstacle in a map, distances from all the robot spheres approximation
+
                 for sphere_i in range(len(map_param.robot_sphere_radius)):
                     obs_fun = CasadiObstacle().simpleFormulation()(robot_sphere_origin_absolute[:, sphere_i],
                                                                    obs_origin_par,
-                                                                   map_param.robot_sphere_radius[sphere_i],
-                                                                   map_param.obstacle_radius)
+                                                                   self.robot_sphere_par_dict[layer_name][sphere_i],
+                                                                   obs_radius_par)
 
                     self.f_obs_grid += obs_weight_par * utils.utils.barrier(obs_fun)
-                    self.obstacle_distances[layer_name][f"sphere_{sphere_i}"].append(obs_fun)
+                    # self.obstacle_distances[layer_name][f"sphere_{sphere_i}"].append(obs_fun)
 
             i_map += 1
 
@@ -151,8 +250,7 @@ class ObstacleGeneratorWrapper:
 
         for layer_name, map_param in self.map_parameters.items():
 
-            if map_param.robot_sphere_publisher_name not in self.robot_markers:
-                self.robot_markers[map_param.robot_sphere_publisher_name] = MarkerArray()
+            self.robot_markers[layer_name] = MarkerArray()
 
             layer_rgb = [random.uniform(0, 1), random.uniform(0, 1), random.uniform(0, 1)]
 
@@ -180,7 +278,16 @@ class ObstacleGeneratorWrapper:
                 robot_marker.color.g = layer_rgb[1]
                 robot_marker.color.b = layer_rgb[2]
 
-                self.robot_markers[map_param.robot_sphere_publisher_name].markers.append(robot_marker)
+                self.robot_markers[layer_name].markers.append(robot_marker)
+
+    def __update_markers(self):
+
+        for layer_name, layer_info in self.map_parameters.items():
+            for sphere_i in range(len(layer_info.robot_sphere_radius)):
+
+                self.robot_markers[layer_name].markers[sphere_i].scale.x = 2 * layer_info.robot_sphere_radius[sphere_i]  # diameter
+                self.robot_markers[layer_name].markers[sphere_i].scale.y = 2 * layer_info.robot_sphere_radius[sphere_i]  # diameter
+
 
     def __init_ros_publisher(self):
         self.robot_pub = dict()
@@ -195,6 +302,16 @@ class ObstacleGeneratorWrapper:
 
         tic_obstacle = time.time()
         obs_vec = dict()
+
+        for layer_name, obstacle_layer in self.obstacle_generator.items():
+            if self.variable_change_flag[layer_name]['max_obs_num']:
+                obstacle_layer.setMaxObstacleNum(self.map_parameters[layer_name].max_obs_num)  # maximum number of obstacle allowed
+                self.variable_change_flag[layer_name]['max_obs_num'] = False
+            if self.variable_change_flag[layer_name]['obstacle_radius']:
+                obstacle_layer.setObstacleRadius(self.map_parameters[layer_name].obstacle_radius)  # radius of obstacle
+                self.variable_change_flag[layer_name]['obstacle_radius'] = False
+
+
         for generator_name, generator in self.obstacle_generator.items():
             # tic_single_generator_time = time.time()
             generator.run()
@@ -218,26 +335,41 @@ class ObstacleGeneratorWrapper:
 
         # assign params
         for map_name, map_params in self.map_parameters.items():
+
+            self.robot_sphere_par_dict[map_name].assign(map_params.robot_sphere_radius)
+
             for obs_i_num in range(map_params.max_obs_num):
                 if obs_i_num < len(obs_vec[map_name]):
                     # update obatacle origin with the sensed one. Needs to be transformed into base_link frame
                     obs_origin_sensed = base_pos + base_rot @ obs_vec[map_name][obs_i_num].getOrigin()
 
+                    self.obs_radius_par_dict[map_name][obs_i_num].assign(map_params.obstacle_radius)
                     self.obs_weight_par_dict[map_name][obs_i_num].assign(map_params.weight_cost_obs)
                     self.obs_origin_par_dict[map_name][obs_i_num].assign(obs_origin_sensed[:2])
 
         time_obstacle_assign = time.time() - tic_assign
         print("time to assign values to obstacles: ", time_obstacle_assign)
 
+        self.__update_markers()
+
+        # ====================================================================
+        merged_array = dict()
         for publisher_name in self.robot_pub:
-            self.robot_pub[publisher_name].publish(self.robot_markers[publisher_name])
+            merged_array[publisher_name] = MarkerArray()
+
+
+        for layer_name, marker_array in self.robot_markers.items():
+            merged_array[self.map_parameters[layer_name].robot_sphere_publisher_name].markers += marker_array.markers
+
+        for publisher_name in self.robot_pub:
+            self.robot_pub[publisher_name].publish(merged_array[publisher_name])
 
         time_obstacles = time.time() - tic_obstacle
         print("time to handle obstacles: ", time_obstacles)
         # self.time_obstacles_list.append(time_obstacles)
 
-    def getObstacleDistances(self):
-        return self.obstacle_distances
+    # def getObstacleDistances(self):
+    #     return self.obstacle_distances
 
     def getObstacleWeightParameter(self):
         return self.obs_weight_par_dict
